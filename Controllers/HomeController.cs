91ecac28b3e915f2ebe6661ea9b8249d25384a12
anjalijawalge12345 <anjali.jawalge@emergys.com>
2025-02-27 12:36:16 +0530

    added map in dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using PinMapper.Web.Models;
 using PinMapper.Web.ViewModels;
 
@@ -8,10 +10,14 @@ namespace PinMapper.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, HttpClient httpClient, IConfiguration configuration)
     {
         _logger = logger;
+        _httpClient = httpClient;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
@@ -25,9 +31,29 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Dashboard()
+    public async Task<IActionResult> Dashboard()
     {
          ViewData["ActivePage"] = "Dashboard";
+
+        var pincodeInfo = new Dictionary<string, string>
+                    {
+                        { "110001", "Connaught Place, New Delhi" },
+                        { "400001", "Fort, Mumbai" },
+                        { "560001", "MG Road, Bangalore" }
+                    };
+
+
+        var locations = new List<PincodeLocation>();
+
+        foreach (var pincode in pincodeInfo)
+        {
+            var (lat, lon) = await GetCoordinatesAsync(pincode.Key);
+            if (lat != 0 && lon != 0)
+            {
+                locations.Add(new PincodeLocation { Pincode = pincode.Key, Latitude = lat, Longitude = lon, Information = pincodeInfo[pincode.Key] });
+            }
+        }
+        ViewBag.GoogleMapsApiKey = _configuration["GoogleMaps:ApiKey"];
 
         var model = new DashboardViewModel()
         {
@@ -35,10 +61,30 @@ public class HomeController : Controller
             States = 280,
             Districts = 400,
             Tehsils = 200,
-            Pins = 19100
+            Pins = 19100,
+            PincodeLocations = locations,
         };
 
         return View(model);
+    }
+
+    private async Task<(double, double)> GetCoordinatesAsync(string pincode)
+    {
+        var apiKey = _configuration["OpenCage:ApiKey"];
+        string url = $"https://api.opencagedata.com/geocode/v1/json?q={pincode},India&key={apiKey}";
+
+        var response = await _httpClient.GetStringAsync(url);
+        var json = JObject.Parse(response);
+
+        var results = json["results"]?.FirstOrDefault();
+        if (results != null)
+        {
+            double lat = (double)results["geometry"]["lat"];
+            double lon = (double)results["geometry"]["lng"];
+            return (lat, lon);
+        }
+
+        return (0, 0);
     }
     public IActionResult PincodeList()
     {
--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -7,4 +7,13 @@ public class DashboardViewModel
     public int Districts { get; set; }
     public int Tehsils { get; set; }
     public int Pins { get; set; }
+    public List<PincodeLocation> PincodeLocations { get; set; }
+}
+
+public class PincodeLocation
+{
+    public string Pincode { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Information { get; set; }
 }
